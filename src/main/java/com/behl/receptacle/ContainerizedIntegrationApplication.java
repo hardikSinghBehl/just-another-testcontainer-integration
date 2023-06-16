6643719e43bc6121846b0389a08ba25c5450346b
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2023-06-16 20:00:57 +0530

    project initialization

---
+++ src/main/java/com/behl/receptacle/ContainerizedIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.behl.receptacle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerizedIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerizedIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/behl/receptacle/ContainerizedIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.behl.receptacle;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+class ContainerizedIntegrationApplicationTests {
+
+  @Test
+  void contextLoads() {
+    assertThat(Boolean.TRUE).isTrue();
+  }
+
+}

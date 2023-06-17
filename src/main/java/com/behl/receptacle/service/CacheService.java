a3d9b3d242beb27546ea8b02fa077069d395acdf
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2023-06-17 13:03:19 +0530

    adding log statements in CacheService.java

--- src/main/java/com/behl/receptacle/service/CacheService.java
+++ src/main/java/com/behl/receptacle/service/CacheService.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CacheService {
@@ -17,23 +19,28 @@ public class CacheService {
 
     public void set(@NonNull final String key, @NonNull final Object value, @NonNull final Duration timeToLive) {
         redisTemplate.opsForValue().set(key, value, timeToLive);
+        log.info("Cached value with key '{}' for {} seconds", key, timeToLive.toSeconds());
     }
     
     public void update(@NonNull final String key, @NonNull final Object value) {
         redisTemplate.opsForValue().setIfPresent(key, value);
+        log.info("Updated cached value with key '{}'", key);
     }
 
     public <T> Optional<T> fetch(@NonNull final String key, @NonNull final Class<T> targetClass) {
         final var value = Optional.ofNullable(redisTemplate.opsForValue().get(key));
         if (value.isEmpty()) {
+            log.info("No cached value found for key '{}'", key);
             return Optional.empty();          
         }
         T result = objectMapper.convertValue(value.get(), targetClass);
+        log.info("Fetched cached value with key '{}'", key);
         return Optional.of(result);
     }
     
     public void delete(@NonNull final String key) {
         redisTemplate.delete(key);
+        log.info("Deleted cached value with key '{}'", key);
     }
 
 }

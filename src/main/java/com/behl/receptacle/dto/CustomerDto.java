3fa05f447b06cce124bc3030ee93bf04849d175f
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2023-06-18 18:09:40 +0530

    adding kafka listener

---
+++ src/main/java/com/behl/receptacle/dto/CustomerDto.java
@@ -0,0 +1,15 @@
+package com.behl.receptacle.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomerDto {
+  
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+
+}
---
+++ src/main/java/com/behl/receptacle/listener/CustomerRegisteredEventListener.java
@@ -0,0 +1,35 @@
+package com.behl.receptacle.listener;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import com.behl.receptacle.configuration.KafkaConfiguration;
+import com.behl.receptacle.dto.CustomerDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+@EnableConfigurationProperties(KafkaConfiguration.class)
+public class CustomerRegisteredEventListener {
+  
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final KafkaConfiguration kafkaConfiguration;
+
+    @SneakyThrows
+    @KafkaListener(topics = "${com.behl.receptacle.kafka.topic-name.customer-registered-event}",
+            groupId = "customer-registered-event-consumer")
+    public void execute(final String message) {
+        final var customerDto = new ObjectMapper().readValue(message, CustomerDto.class);
+        log.info("Received confirmation of account registration event for customer {}", customerDto.getId());
+        
+        final var initiateRiskAssessmentTopic = kafkaConfiguration.getCustomerAccountRiskAssessment();
+        kafkaTemplate.send(initiateRiskAssessmentTopic, customerDto);
+        log.info("Successfully sent command to initiate risk assessment for customer {}", customerDto.getId());
+    }
+
+}

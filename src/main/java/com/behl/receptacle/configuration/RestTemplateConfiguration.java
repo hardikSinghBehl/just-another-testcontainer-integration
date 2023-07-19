65c786b685767ba2d18cd91191e73dc9215fccd2
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2023-07-19 10:03:32 +0530

    reducing RestTemplate timeout configuration

--- src/main/java/com/behl/receptacle/configuration/RestTemplateConfiguration.java
+++ src/main/java/com/behl/receptacle/configuration/RestTemplateConfiguration.java
@@ -12,8 +12,8 @@ public class RestTemplateConfiguration {
     @Bean
     public RestTemplate restTemplate(final RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder
-            .setConnectTimeout(Duration.ofSeconds(30))
-            .setReadTimeout(Duration.ofSeconds(30))
+            .setConnectTimeout(Duration.ofSeconds(10))
+            .setReadTimeout(Duration.ofSeconds(10))
             .build();
     }
 
